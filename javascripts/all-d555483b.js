7ab6ecad66b2a2e4e04f35c09af13fe507ae578c
Luke Baker <lukebaker@gmail.com>
2016-11-04 16:36:55 +0000

    rebuild pages at 9e2003b

--- javascripts/all-d555483b.js
+++ javascripts/all-d555483b.js
@@ -15,7 +15,6 @@ $(document).ready(function() {
       }
       window.annotator.on('annotationCreated', logEvent.bind(null, 'created'));
       window.annotator.on('annotationUpdated', logEvent.bind(null, 'updated'));
-      window.annotator.on('annotationDeleted', logEvent.bind(null, 'deleted'));
 
       $('button[name="sidebar-toggle"], button[name="highlight-visibility"]').on('click', function(ev) {
         window.ga('send', 'event', 'Annotation', ev.currentTarget.name + '-click');
@@ -94,6 +93,15 @@ $(document).ready(function() {
     if (closed) {
       $el.remove();
     }
+  });
+  // Hide all email asks if they've already submitted their email address.
+  if (document.cookie.replace(/(?:(?:^|.*;\s*)submittedEmail\s*\=\s*([^;]*).*$)|^.*$/, "$1") === "true") {
+    $('.email-cta').not('.always-show').addClass('hide');
+  }
+
+  $(document).on('click', '.close-bottom-cta', function(ev) {
+    ev.preventDefault();
+    $(ev.currentTarget).closest('.email-cta').addClass('hide');
   });
 
   // Stash cookie with alert name if alert is closed.
